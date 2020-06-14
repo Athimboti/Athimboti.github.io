4f2674d7cbdc351f8b7a77669402eed04b3f467d
Athenkosi1565 <Athimboti2@gmail.com>
2020-06-14 23:20:30 +0200

    first commit

---
+++ js/my-jvascript.js
@@ -0,0 +1,17 @@
+var navigat = document.getElementById("main-menu li a");
+navigat.innerHTML = Str;
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+
+		
+var navbar = document.getElementById("navbar").style.color = 'Orange';
+var navbar = document.getElementById("navbar").style.fontFamily = '';
+var navbar = document.getElementById("navbar").style.color = 'orange';
+var sticky = navbar.offsetTop;
+
+function navbar (navbar) {
+    var square = document.getElementById("squre");
+    square.style.backgroundColor = "transparent";
+}
