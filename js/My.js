86ccea18b4407bf437c7c77b2a0a6701363e21f1
Athimboti <44023487+Athimboti@users.noreply.github.com>
2020-07-10 10:24:57 +0200

    fetch users
    on github

--- js/My.js
+++ js/My.js
@@ -1,4 +1,27 @@
- 
+async function getUsers(names) {
+  let jobs = [];
+
+  for(let name of names) {
+    let job = fetch(`https://api.github.com/users/${name}`).then(
+      successResponse => {
+        if (successResponse.status != 200) {
+          return null;
+        } else {
+          return successResponse.json();
+        }
+      },
+      failResponse => {
+        return null;
+      }
+    );
+    Jobs.push(job);
+  }
+
+  let results = await Promise.all(jobs);
+
+  return results;
+}
+
 
 function nav () {
     var navbar = document.getElementById("navbar");
